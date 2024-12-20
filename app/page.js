7e06974036189dbc5f73e6ca1791aed030b0ed0e
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-12-20 17:40:43 +0500

    added HoverFlowMap

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
-import HoverFlowmap from "@/components/HoverFlowMap";
+import Experience from "@/components/Experience";
 
 export default function Home() {
     return (
-            <div className="w-screen h-screen bg-black text-white text-9xl flex items-center justify-center">
-
-            <HoverFlowmap />
+        <div className="w-screen h-screen bg-black text-white text-9xl flex items-center justify-center">
+            <Experience />
         </div>
     );
 }
---
+++ components/Experience.jsx
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react'
+import { Canvas } from 'react-ogl';
+import HoverFlowMap from './HoverFlowMap';
+
+const Experience = () => {
+    return (
+        <Canvas dpr={2} renderer={{ alpha: true, premultipliedAlpha: true }} style={{width: "100%", height: "100%", backgroundColor: "black"}}>
+            <HoverFlowMap />
+        </Canvas>
+    )
+}
+
+export default Experience
